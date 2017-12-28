6ded505569cfb1aa3c758bbec67d4c20f2260681
hirothings <sakamoto.hiroshi@moneyforward.co.jp>
2017-12-28 16:09:25 +0900

    project構成変更

---
+++ FavotterDomain/FavotterDomain.h
@@ -0,0 +1,19 @@
+//
+//  FavotterDomain.h
+//  FavotterDomain
+//
+//  Created by 坂本 浩 on 2017/12/28.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FavotterDomain.
+FOUNDATION_EXPORT double FavotterDomainVersionNumber;
+
+//! Project version string for FavotterDomain.
+FOUNDATION_EXPORT const unsigned char FavotterDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FavotterDomain/PublicHeader.h>
+
+
