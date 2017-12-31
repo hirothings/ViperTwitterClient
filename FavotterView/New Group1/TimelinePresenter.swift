c183995ee8974b85eab9440457437c74a690978a
hirothings <hiroshings.job1987@gmail.com>
2017-12-31 17:54:50 +0900

    ファイル移動

---
+++ FavotterView/New Group1/TimelinePresenter.swift
@@ -0,0 +1,51 @@
+//
+//  TweetsPresenter.swift
+//  FavotterView
+//
+//  Created by hirothings on 2017/12/31.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import FavotterDomain
+import FavotterAPIClient
+
+protocol TimelinePresentation {
+    weak var view: TimelineView? { get }
+    var interactor: TweetsInteractorInput { get }
+    var router: TweetsWireframe { get }
+    
+    func viewDidLoad()
+}
+
+class TimelinePresenter: TimelinePresentation {
+    weak var view: TimelineView?
+    var router: TimelineWireframe
+    var interactor: TweetsInteractorInput!
+    var output: TweetsInteractorOutput!
+    var tweets: [Tweet] = []
+    
+    private let bag = DisposeBag()
+    
+    func viewDidLoad() {
+        interactor.fetch(with: "hirothings")
+            .subscribe(
+                onNext: { [weak self] (tweets: [Tweet]) in
+                    self?.output.tweetsFetched(tweets)
+                },
+                onError: { [weak self] _ in
+                    self?.output.tweetsFetchFailed()
+                }
+            )
+            .disposed(by: bag)
+    }
+}
+
+extension TimelinePresenter: TweetsInteractorOutput {
+    func tweetsFetched(_ tweets: [Tweet]) {
+    }
+    
+    func tweetsFetchFailed() {
+    }
+}
