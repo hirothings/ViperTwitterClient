528f9bb52979465282894428e79f261a8a711a6d
hirothings <hiroshings.job1987@gmail.com>
2017-12-31 18:15:28 +0900

    LoginViewController 追加

--- FavotterView/View/LoginViewController.swift
+++ FavotterView/View/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Favotter
 //
 //  Created by hirothings on 2017/12/19.
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import TwitterKit
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = TWTRLogInButton { [weak self] (session: TWTRSession?, error: Error?) in
+            guard let `self` = self else { return }
+            if let error = error {
+                let nsError = error as NSError
+                if nsError.code == 1 { return }
+                // TODO: Use Router
+//                self.showError(error: TwitterAPIError(error: nsError))
+            }
+            else {
+                // TODO: Use Router
+                let timelineVC = self.storyboard!.instantiateViewController(withIdentifier: "TimeLineViewController")
+                let nvc = UINavigationController(rootViewController: timelineVC)
+                self.present(nvc, animated: true, completion: nil)
+            }
+        }
+        
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
