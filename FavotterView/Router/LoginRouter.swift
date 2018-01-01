2701914011125c9761a4286f8e4c1f78ac482ce9
hirothings <hiroshings.job1987@gmail.com>
2018-01-01 17:45:48 +0900

    Loginのrouter実装
    TODO: Presenter

--- FavotterView/Router/LoginRouter.swift
+++ FavotterView/Router/LoginRouter.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+protocol LoginRouterWireframe {
+    weak var viewController: UIViewController? { get set }
+    func pushTimelineView(userID: String)
+    static func assembleModule() -> UIViewController
+}
+
+class LoginRouter: LoginRouterWireframe {
+    weak var viewController: UIViewController?
+    
+    static func assembleModule() -> UIViewController {
+        let storyBoard = UIStoryboard(name: "LoginViewController", bundle: nil)
+        let view = storyBoard.instantiateInitialViewController() as! LoginViewController
+        let nav = UINavigationController(rootViewController: view)
+        // TODO: Use Presenter
+        
+        return nav
+    }
+    
+    func pushTimelineView(userID: String) {
+    }
+}
