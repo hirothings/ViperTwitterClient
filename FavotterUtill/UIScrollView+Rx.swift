e27df318812fee716a5ca6af733a6aa6f76f26dc
hirothings <hiroshings.job1987@gmail.com>
2018-01-12 03:06:44 +0900

    Add RxReachedBottom

--- FavotterUtill/UIScrollView+Rx.swift
+++ FavotterUtill/UIScrollView+Rx.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018年 hirothings. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UIScrollView {
+    public var reachedBottom: ControlEvent<Void> {
+        let observable = contentOffset
+            .flatMap { [weak base] contentOffset -> Observable<Void> in
+                guard let scrollView = base else {
+                    return Observable.empty()
+                }
+                
+                let visibleHeight = scrollView.frame.height - scrollView.contentInset.top - scrollView.contentInset.bottom
+                let y = contentOffset.y + scrollView.contentInset.top
+                let threshold = max(0.0, scrollView.contentSize.height - visibleHeight)
+                
+                return y > threshold ? Observable.just(()) : Observable.empty()
+        }
+        
+        return ControlEvent(events: observable)
+    }
+}
