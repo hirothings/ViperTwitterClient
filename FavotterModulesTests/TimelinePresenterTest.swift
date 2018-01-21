08d9d1a52c8b345501442cda3230a7e8de22af1f
hirothings <hiroshings.job1987@gmail.com>
2018-01-21 18:44:11 +0900

    テスト用のモック、スタブ作成

--- FavotterModulesTests/TimelinePresenterTest.swift
+++ FavotterModulesTests/TimelinePresenterTest.swift
@@ -7,6 +7,47 @@
 //
 
 import XCTest
+import RxSwift
+import TwitterKit
+
+@testable import FavotterModel
+@testable import FavotterModules
+
+enum TweetMock {
+    case none
+    case initialTweets
+    case additionalTweets
+    // memo: 画像付きのツイートなどパターンはここに追加していく(UITestでも使えそう)
+    
+    func generate() -> [Tweet] {
+        switch self {
+        case .none:
+            return []
+        case .initialTweets:
+            return Array(1..<20).map {
+                let user = User(name: "ユーザー\($0)", screenName: "test_\($0)", description: "", profileImageURL: "https://test\($0).com", profileBGImageURL: "https://test\($0).com", friendsCount: $0, followersCount: $0)
+                return Tweet(id: Int64($0), text: "こんにちは！\($0)", user: user, retweetCount: $0, favCount: $0)
+            }
+        case .additionalTweets:
+            return Array(1..<20).map {
+                let user = User(name: "追加ユーザー\($0)", screenName: "test_\($0)", description: "", profileImageURL: "https://test\($0).com", profileBGImageURL: "https://test\($0).com", friendsCount: $0, followersCount: $0)
+                return Tweet(id: Int64($0), text: "追加ユーザーです。こんにちは！\($0)", user: user, retweetCount: $0, favCount: $0)
+            }
+        }
+    }
+}
+
+class TimelineUseCaseStub: TimelineUsecase {
+    var mock: TweetMock!
+    
+    func fetch(with userID: String) -> Observable<[Tweet]> {
+        return Observable.just(mock.generate())
+    }
+    
+    func addTweets(userID: String, maxID: Int64) -> Observable<[Tweet]> {
+        return Observable.just(mock.generate())
+    }
+}
 
 class TimelinePresenterTest: XCTestCase {
     
