712a0649cd5acb8e7d2168cb0eb7b9d48a9b23a1
hirothings <hiroshings.job1987@gmail.com>
2018-02-17 18:22:43 +0900

    Tweet画像の表示

--- FavotterModel/Entity/Tweet.swift
+++ FavotterModel/Entity/Tweet.swift
@@ -14,13 +14,33 @@ public struct Tweet: Codable {
     public let user: User
     public let retweetCount: Int
     public let favCount: Int
+    public let media: [Media]?
     
-    public init(id: Int64, text: String, user: User, retweetCount: Int, favCount: Int) {
-        self.id = id
-        self.text = text
-        self.user = user
-        self.retweetCount = retweetCount
-        self.favCount = favCount
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(Int64.self, forKey: .id)
+        text = try values.decode(String.self, forKey: .text)
+        user = try values.decode(User.self, forKey: .user)
+        retweetCount = try values.decode(Int.self, forKey: .retweetCount)
+        favCount = try values.decode(Int.self, forKey: .favCount)
+
+        if let entities = try? values.nestedContainer(keyedBy: ExtendedEntitiesKeys.self, forKey: .extendedEntities) {
+            media = try entities.decode([Media]?.self, forKey: .media)
+        } else {
+            media = nil
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(text, forKey: .text)
+        try container.encode(user, forKey: .user)
+        try container.encode(retweetCount, forKey: .retweetCount)
+        try container.encode(favCount, forKey: .favCount)
+
+        var entities = container.nestedContainer(keyedBy: ExtendedEntitiesKeys.self, forKey: .extendedEntities)
+        try entities.encode(media, forKey: .media)
     }
     
     private enum CodingKeys: String, CodingKey {
@@ -29,5 +49,10 @@ public struct Tweet: Codable {
         case user
         case retweetCount = "retweet_count"
         case favCount = "favorite_count"
+        case extendedEntities = "extended_entities"
+    }
+    
+    private enum ExtendedEntitiesKeys: String, CodingKey {
+        case media
     }
 }
--- FavotterModules/Timeline/View/TableViewCell/TweetTableViewCell.swift
+++ FavotterModules/Timeline/View/TableViewCell/TweetTableViewCell.swift
@@ -20,6 +20,7 @@ class TweetTableViewCell: UITableViewCell, NibReusable {
     @IBOutlet weak var tweetLabel: UILabel!
     @IBOutlet weak var retweetCount: UILabel!
     @IBOutlet weak var fabCount: UILabel!
+    @IBOutlet weak var tweetImageVIew: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -31,11 +32,49 @@ class TweetTableViewCell: UITableViewCell, NibReusable {
     func setupTweet(_ tweet: Tweet) {
         if let url = URL(string: tweet.user.profileImageURL) {
             Manager.shared.loadImage(with: url, into: profileImageView)
+        } else {
+            profileImageView.image = nil
+        }
+        if let urlStr = tweet.media?.first?.url, let url = URL(string: urlStr) {
+            Manager.shared.loadImage(with: url, into: tweetImageVIew, handler: { [weak self] (image: Result<UIImage>, _) in
+                guard let weakSelf = self, let image = image.value else { return }
+                weakSelf.tweetImageVIew.image = weakSelf.cropImage(
+                    image: image,
+                    width: weakSelf.tweetImageVIew.frame.width,
+                    height: weakSelf.tweetImageVIew.frame.width / 2
+                )
+            })
+        } else {
+            tweetImageVIew.image = nil
         }
         nameLabel.text = tweet.user.name
         screenNameLabel.text = "@\(tweet.user.screenName)"
         tweetLabel.text = tweet.text
         retweetCount.text = "\(tweet.retweetCount)"
         fabCount.text = "\(tweet.favCount)"
+    }
+    
+    private func cropImage(image: UIImage, width w: CGFloat, height h: CGFloat) -> UIImage {
+        let originRef = image.cgImage
+        let originWidth = CGFloat(originRef!.width)
+        let originHeight = CGFloat(originRef!.height)
+        var resizeWidth: CGFloat
+        var resizeHeight: CGFloat
+        
+        resizeWidth = w
+        resizeHeight = originHeight * resizeWidth / originWidth
+        
+        let resizeSize = CGSize(width: resizeWidth, height: resizeHeight)
+        UIGraphicsBeginImageContext(resizeSize)
+        
+        image.draw(in: CGRect(x: 0, y: 0, width: resizeWidth, height: resizeHeight))
+        let resizeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let cropRect = CGRect(x: (resizeWidth - w) / 2,
+                              y: (resizeHeight - h) / 2,
+                              width: w, height: h)
+        let cropRef = resizeImage!.cgImage!.cropping(to: cropRect)
+        return UIImage(cgImage: cropRef!)
     }
 }
